7adb402a454d44a6c5658f66ffe0955d686a6549
abhinav-shr <abhinavsharma99962@gmail.com>
2021-05-28 03:18:51 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const search = document.querySelector(".search");
+const btn = document.querySelector('button');
+
+let count = 0;
+btn.addEventListener('click',() => {
+    count++;
+    update();
+});
+
+function update(){
+    if(count===1){
+        search.classList.add('active');
+        btn.classList.add('close');
+        console.log(search);
+        console.log(btn);
+        console.log(count);
+    };
+    if(count===2){
+        search.classList.remove('active');
+        btn.classList.remove('close');
+        
+        console.log(search);
+        count--;
+        count--;
+        console.log(count);
+    };      
+};
+
